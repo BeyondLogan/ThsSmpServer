661bf7e1a882a95186ba7caaf46cf73f74ac6099
BeyondLogan <103182857+BeyondLogan@users.noreply.github.com>
2022-05-26 12:24:37 +0930

    Update Setup.sh

--- Setup.sh
+++ Setup.sh
@@ -9,10 +9,5 @@ mkdir MinecraftServer
 cd MinecraftServer
 apt install wget
 wget https://download.getbukkit.org/spigot/spigot-1.18.2.jar
-mkdir Plugins
-cd Plugins
-wget https://www.spigotmc.org/resources/viabackwards.27448/download?version=445618
-wget https://www.spigotmc.org/resources/viaversion.19254/download?version=443043
-wget https://dev.bukkit.org/projects/sec-anti-spawn-kill-plugin-v1-3/files/latest
-wget https://dev.bukkit.org/projects/clearlagg/files/latest
+git clone https://github.com/BeyondLogan/Plugins.git
 cd
