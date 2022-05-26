adf490bc041d1ab00d70ad44412c6f208ee508bd
BeyondLogan <103182857+BeyondLogan@users.noreply.github.com>
2022-05-26 12:02:33 +0930

    Update Setup.sh

--- Setup.sh
+++ Setup.sh
@@ -9,4 +9,5 @@ cd Plugins
 wget https://www.spigotmc.org/resources/viabackwards.27448/download?version=445618
 wget https://www.spigotmc.org/resources/viaversion.19254/download?version=443043
 wget https://dev.bukkit.org/projects/sec-anti-spawn-kill-plugin-v1-3/files/latest
+wget https://dev.bukkit.org/projects/clearlagg/files/latest
 cd
