b651f77828335366e505aad3b4e9ac6f12364f5e
BeyondLogan <103182857+BeyondLogan@users.noreply.github.com>
2022-05-30 08:49:35 +0930

    Update Setup.sh

--- Setup.sh
+++ Setup.sh
@@ -8,3 +8,5 @@ apt install wget
 wget https://download.getbukkit.org/spigot/spigot-1.18.2.jar
 git clone https://github.com/BeyondLogan/Plugins.git
 cd
+cp ThsSmpServer/StartUp.sh ThsSmpServer/MinecraftServer/StartUp.sh
+cd
